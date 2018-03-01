76c52a0e0d50b839f3d041ddeb42c10b65f5ef68
Егор Пилипенко <egor.pilipenko.1998@gmail.com>
2018-03-02 00:15:55 +0300

    matrix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function solveSudoku(matrix) {
-    var matrixProt = matrix;
+    var matxpro = matrix;
 
-    function replOfPasses(matrix) {
+    function passes(matrix) {
         for (let i = 0; i < matrix.length; i++) {
             for (let j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
@@ -13,40 +13,40 @@ module.exports = function solveSudoku(matrix) {
     }
 
 
-    function goOverLine(matrixProt) {
+    function forLine(matx) {
         var arrPos = [];
-        for (let i = 0; i < matrixProt.length; i++) {
-            for (let j = 0; j < matrixProt[i].length; j++) {
-                if (typeof matrixProt[i][j] == 'object') {
+        for (let i = 0; i < matx.length; i++) {
+            for (let j = 0; j < matx[i].length; j++) {
+                if (typeof matx[i][j] == 'object') {
                     arrPos.push(j);
                 }
             }
             for (let j = 0; j < arrPos.length; j++) {
-                for (let l = 0; l < matrixProt[i][arrPos[j]].length; l++) {
-                    for (let k = 0; k < matrixProt[i].length; k++) {
-                        if (matrixProt[i][arrPos[j]][l] == matrixProt[i][k]) {
-                            matrixProt[i][arrPos[j]].splice(l, 1);
-                            if (matrixProt[i][arrPos[j]].length == 1) {
-                                matrixProt[i][arrPos[j]] = matrixProt[i][arrPos[j]][0];
+                for (let l = 0; l < matx[i][arrPos[j]].length; l++) {
+                    for (let k = 0; k < matx[i].length; k++) {
+                        if (matx[i][arrPos[j]][l] == matx[i][k]) {
+                            matx[i][arrPos[j]].splice(l, 1);
+                            if (matx[i][arrPos[j]].length == 1) {
+                                matx[i][arrPos[j]] = matx[i][arrPos[j]][0];
                             }
                         }
                     }
                 }
             }
         }
-        return matrixProt;
+        return matx;
     }
 
-    function goOverColumn(matrixProt) {
+    function forColumn(matx) {
         for (let i = 0; i < 9; i++) {
             for (let j = 0; j < 9; j++) {
-                if (typeof matrixProt[i][j] == "object") {
+                if (typeof matx[i][j] == "object") {
                     for (let g = 0; g < 9; g++) {
-                        for (let k = 0; k < matrixProt[i][j].length; k++) {
-                            if (matrixProt[i][j][k] == matrixProt[g][j] && typeof matrixProt[g][j] != "object") {
-                                matrixProt[i][j].splice(k, 1);
-                                if (matrixProt[i][j].length == 1) {
-                                    matrixProt[i][j] = matrixProt[i][j][0];
+                        for (let k = 0; k < matx[i][j].length; k++) {
+                            if (matx[i][j][k] == matx[g][j] && typeof matx[g][j] != "object") {
+                                matx[i][j].splice(k, 1);
+                                if (matx[i][j].length == 1) {
+                                    matx[i][j] = matx[i][j][0];
                                 }
                             }
                         }
@@ -54,9 +54,9 @@ module.exports = function solveSudoku(matrix) {
                 }
             }
         }
-        return matrixProt;
+        return matx;
     }
 
-    return goOverColumn(goOverLine(replOfPasses(matrixProt)));
+    return forColumn(forLine(passes(matxpro)));
 
 }
